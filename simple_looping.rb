488af42149bba9c780d3782bd75d1a7c00f90a9d
Eliot Howes <ejhowes@gmail.com>
2019-05-20 22:38:26 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -53,9 +53,9 @@ def for_iterator(number_of_times)
 
   phrase = "Welcome to Flatiron School's Web Development Course!"
 
-  for number in number_of_times do
-    puts phrase
-  end
+    for number in number_of_times do
+      puts phrase
+    end
   end
 
 end
